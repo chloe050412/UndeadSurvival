af42ebd7661e760197462d99d90b45c0eff2771d
chloe050412 <chloe980504@gmail.com>
2024-07-29 17:40:06 +0900

    플레이어 이동

---
+++ UndeadSurvival/Assets/Script/Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer mesh;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputVec = transform.position;
+        rigid = GetComponent<Rigidbody2D>();
+        mesh = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    inputVec.x = Input.GetAxisRaw("Horizontal");
+    //    inputVec.y = Input.GetAxisRaw("Vertical");
+    //}
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        // 움직이고자 하는 방향을 정규화 * 속도 * dt
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        animator.SetFloat("Speed", inputVec.magnitude);
+       
+        if (inputVec.x != 0)
+            mesh.flipX = inputVec.x < 0;
+
+    }
+}
